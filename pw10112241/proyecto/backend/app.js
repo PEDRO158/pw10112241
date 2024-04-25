5ab8cd2bc592f12dca7c8300af6fefe36a60d04a
PEDRO158 <159815839+PEDRO158@users.noreply.github.com>
2024-04-25 10:50:47 -0700

    Add files via upload

--- pw10112241/proyecto/backend/app.js
+++ pw10112241/proyecto/backend/app.js
@@ -6,10 +6,37 @@ let app=express();
 app.use(express.json()); //para marder mensaje tipo json
 app.use(cors());//aceptar solicitudes de un servidor externo
 
+//conexion a MySQL
+let conexion=mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+});
+//nos conectamos a mysql
+conexion.connect(function(error){
+    if (error) {
+        throw error;
+    }else{
+        console.log('Conectado a la base de datos');
+    }
+});
+
 //rutas de acceso
 app.get("/",function(req,res) {
     res.send("Ruta de inicio");
 });
+//seleccionamos todos los clientes
+app.get('/api/clientes',(req,res)=>{
+    conexion.query('SELECT * FROM clientes',(error,filas)=>{
+        if (error) {
+            throw error;
+        } else {
+            res.send(filas);
+        }
+    })
+})
 
 //encender servidor
 let puerto=3000;    //estableer un puerto
