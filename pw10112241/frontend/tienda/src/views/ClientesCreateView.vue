ec33155b0d0ee5fbdc8649c6110c6e5e481ec8e3
PEDRO158 <159815839+PEDRO158@users.noreply.github.com>
2024-05-16 10:55:04 -0700

    Agregar Clientes

--- pw10112241/frontend/tienda/src/views/ClientesCreateView.vue
+++ pw10112241/frontend/tienda/src/views/ClientesCreateView.vue
@@ -3,6 +3,9 @@
         <div class="card">
             <div class="card-header">
                 <h4>Agregar Clientes</h4>
+                <div v-if="mensaje==1" class="alert alert-success" role="alert">
+                    Datos guardados con exito.
+                </div>
             </div>
             <div class="card-body">
                 <div class="mt-3">
@@ -37,14 +40,19 @@
                     CP
                     <input type="text" class="form-control" v-model="model.cliente.cp">
                 </div>
+                <div class="mt-3">
+                    <button class="btn btn-primary" @click="guardarCliente()"> Guardar</button>
+                </div>
             </div>
         </div>
 </template>
 <script>
+import axios from 'axios';
 export default{
     name: "ClientesView",
     data(){
         return{
+            mensaje:0,
             model: {
                 cliente:{
                     id: '',
@@ -57,6 +65,25 @@ export default{
                     cp: ''
                 }
             },
+        }
+    }, methods:{
+        guardarCliente(){
+            axios.post('http://localhost:3000/api/clientes',this.model.cliente)
+            .then(res=>{
+                if (res.data.affectedRows== 1) {//si se inserta un registro
+                    this.model.cliente={
+                        id: '',
+                        nombre: '',
+                        apellido: '',
+                        direccion: '',
+                        telefono: '',
+                        rfc: '',
+                        curp: '',
+                        cp: ''
+                    }
+                    this.mensaje=1;//mensaje de exito
+                }
+            })
         }
     }
 }
